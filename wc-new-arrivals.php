721baeb0c6bd679f2d6862e1ef91cab1c52b5e4a
sportmax64 <info@sportmax.ch>
2026-06-16 22:55:53 +0200

    Add WooCommerce Nouvel Arrivage plugin
    This plugin displays a 'New Arrival' badge on WooCommerce products.

--- wc-new-arrivals.php
+++ wc-new-arrivals.php
@@ -1,1 +1,81 @@
 
+<?php
+/**
+ * Plugin Name: WooCommerce Nouvel Arrivage
+ * Plugin URI: https://github.com/sportmax64/nouvel_arrivage
+ * Description: Affiche automatiquement un badge "Nouvel arrivage" sur les produits WooCommerce.
+ * Version: 0.1.0
+ * Author: Sportmax
+ * Text Domain: wc-new-arrivals
+ * Domain Path: /languages
+ * Requires PHP: 8.1
+ */
+
+defined('ABSPATH') || exit;
+
+define('WNA_VERSION', '0.1.0');
+define('WNA_PLUGIN_FILE', __FILE__);
+define('WNA_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('WNA_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+/*
+|--------------------------------------------------------------------------
+| Autoload
+|--------------------------------------------------------------------------
+*/
+
+spl_autoload_register(
+    static function (string $class): void {
+
+        if (strpos($class, 'WNA\\') !== 0) {
+            return;
+        }
+
+        $class = substr($class, 4);
+
+        $file = WNA_PLUGIN_PATH
+            . 'includes/'
+            . str_replace('\\', DIRECTORY_SEPARATOR, $class)
+            . '.php';
+
+        if (file_exists($file)) {
+            require_once $file;
+        }
+
+    }
+);
+
+/*
+|--------------------------------------------------------------------------
+| Bootstrap
+|--------------------------------------------------------------------------
+*/
+
+add_action(
+    'plugins_loaded',
+    static function () {
+
+        if (!class_exists('WooCommerce')) {
+
+            add_action(
+                'admin_notices',
+                static function () {
+
+                    ?>
+                    <div class="notice notice-error">
+                        <p>
+                            WooCommerce Nouvel Arrivage nécessite WooCommerce.
+                        </p>
+                    </div>
+                    <?php
+
+                }
+            );
+
+            return;
+        }
+
+        WNA\Plugin::instance()->boot();
+
+    }
+);
